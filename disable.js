b67612c3ee7ccbfa106ac18500e5b728ce178cc7
ishmam ali <ishmamalikhan@gmail.com>
2020-08-06 14:55:26 +0500

    disable functionality in javascript

---
+++ disable.js
@@ -0,0 +1,8 @@
+function disableBtn(){
+    var btn=document.getElementById('example');
+    btn.disabled=true;   
+}
+function getElements(){
+    var p=document.getElementsByTagName('p');
+    console.log(p);
+}
